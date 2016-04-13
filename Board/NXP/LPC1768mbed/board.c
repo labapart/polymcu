700c3a2847ea2d00d27315a3f92537cc0369d057
Olivier Martin <olivier@labapart.com>
2016-04-13 09:11:30 +0100

    Board/LPC1768mbed: Clean LED code by using NXP GPIO functions

--- Board/NXP/LPC1768mbed/board.c
+++ Board/NXP/LPC1768mbed/board.c
@@ -26,75 +26,12 @@
 
 #include "internal.h"
 
-typedef struct {
-    PinName  pin;
-    uint32_t mask;
+#define LED_COUNT  4
 
-    __IO uint32_t *reg_dir;
-    __IO uint32_t *reg_set;
-    __IO uint32_t *reg_clr;
-    __I  uint32_t *reg_in;
-} gpio_t;
+static const int g_led_gpio_port_num[LED_COUNT] = { 1,   1,  1,  1 };
+static const int g_led_gpio_bit_num[LED_COUNT]  = { 18, 20, 21, 23 };
 
 extern const ARM_DRIVER_USART Driver_UART_DEBUG;
-
-static gpio_t leds[4];
-
-uint32_t gpio_set(PinName pin) {
-    return (1 << ((int)pin & 0x1F));
-}
-
-void gpio_init(gpio_t *obj, PinName pin) {
-    obj->pin = pin;
-    if (pin == (PinName)NC)
-        return;
-
-    obj->mask = gpio_set(pin);
-    LPC_GPIO_T *port_reg = (LPC_GPIO_T *)((int)pin & ~0x1F);
-    obj->reg_set = &port_reg->SET;
-    obj->reg_clr = &port_reg->CLR;
-    obj->reg_in  = &port_reg->PIN;
-    obj->reg_dir = &port_reg->DIR;
-}
-
-void gpio_mode(gpio_t *obj, PinMode mode) {
-}
-
-void gpio_dir(gpio_t *obj, PinDirection direction) {
-    switch (direction) {
-        case PIN_INPUT :
-            *obj->reg_dir &= ~obj->mask;
-            break;
-        case PIN_OUTPUT:
-            *obj->reg_dir |= obj->mask;
-            break;
-    }
-}
-
-void gpio_write(gpio_t *obj, int value) {
-    if (value)
-        *obj->reg_set = obj->mask;
-    else
-        *obj->reg_clr = obj->mask;
-}
-
-static inline void _gpio_init_out(gpio_t* gpio, PinName pin, PinMode mode, int value)
-{
-    gpio_init(gpio, pin);
-    if (pin != NC) {
-        gpio_write(gpio, value);
-        gpio_dir(gpio, PIN_OUTPUT);
-        gpio_mode(gpio, mode);
-    }
-}
-
-void gpio_init_out_ex(gpio_t* gpio, PinName pin, int value) {
-    _gpio_init_out(gpio, pin, PullNone, value);
-}
-
-void gpio_init_out(gpio_t* gpio, PinName pin) {
-    gpio_init_out_ex(gpio, pin, 0);
-}
 
 /* Pin muxing configuration */
 static const PINMUX_GRP_T pinmuxing[] = {
@@ -108,29 +45,15 @@ static const PINMUX_GRP_T pinmuxing[] = {
 
 // Initialize newlib
 void hardware_init_hook(void) {
+	int i;
+
 	// Setup pin muxings
 	Chip_IOCON_SetPinMuxing(LPC_IOCON, pinmuxing, sizeof(pinmuxing) / sizeof(PINMUX_GRP_T));
 
-	//
-	// Initialize Board LED
-	//
-
-	//Configure GPIOs as output
-	gpio_init_out(&leds[0], LED1);
-	gpio_init_out(&leds[1], LED2);
-	gpio_init_out(&leds[2], LED3);
-	gpio_init_out(&leds[3], LED4);
-
-#if 0
-	/* Pin PIO0_22 is configured as GPIO pin during SystemInit */
-	/* Set the PIO_22 as output */
-	/* Initializes GPIO */
-	Chip_GPIO_Init(LPC_GPIO);
-	Chip_IOCON_Init(LPC_IOCON);
-
-	Chip_GPIO_WriteDirBit(LPC_GPIO, 1, 1 << (LED1 & 0x1F), 1);
-	Chip_GPIO_WritePortBit(LPC_GPIO, 1, 1 << (LED1 & 0x1F), 1);
-#endif
+	// Initialize Board LEDs
+	for (i = 0; i < LED_COUNT; i++) {
+		Chip_GPIO_WriteDirBit(LPC_GPIO, g_led_gpio_port_num[i], g_led_gpio_bit_num[i], true);
+	}
 
 #ifndef SUPPORT_DEBUG_UART_NONE
 	// Initialize UART
@@ -147,8 +70,8 @@ void hardware_init_hook(void) {
 }
 
 void set_led(int led, int value) {
-	if(led < sizeof(leds)/sizeof(gpio_t)) {
-		gpio_write(&leds[led], value);
+	if(led < LED_COUNT) {
+		Chip_GPIO_WritePortBit(LPC_GPIO, g_led_gpio_port_num[led], g_led_gpio_bit_num[led], value);
 	}
 }
 
